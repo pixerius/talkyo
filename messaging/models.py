d79dac944344eaa6ef8b7290fcfc84d0703f10c7
Przemek Rembak <przemyslaw.rembak@10clouds.com>
2017-11-25 16:38:29 +0100

    message and conversation models

--- messaging/models.py
+++ messaging/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Conversation(models.Model):
+    users = models.ManyToManyField('users.User')
+
+
+class Message(models.Model):
+    class Meta:
+        default_related_name = 'messages'
+
+    author = models.ForeignKey('users.User', models.CASCADE,
+                               related_name='messages')
+    conversation = models.ForeignKey('Conversation', models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
